a452d5511e6d82168a54e6f4fd228cbb3ed87cbc
esahcc <ccbiddle94@gmail.com>
2021-09-29 16:32:17 -0700

    FizzBuzz

--- 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -1,6 +1,9 @@
 public class FizzBuzzTest {
     public static void main(String[] args) {
         FizzBuzz myTest=new FizzBuzz();
-        System.out.println(FizzBuzz.fizzBuzz(32));
+        System.out.println(FizzBuzz.fizzBuzz(30));
+        System.out.println(FizzBuzz.fizzBuzz(25));
+        System.out.println(FizzBuzz.fizzBuzz(22));
+        System.out.println(FizzBuzz.fizzBuzz(18));
     }
 }
