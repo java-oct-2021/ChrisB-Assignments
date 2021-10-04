a5cbabdeabe33e489784e884414d62498f83f0a1
esahcc <ccbiddle94@gmail.com>
2021-10-04 16:08:11 -0700

    puzzling

--- 01-java-fundamentals/06-puzzling/puzzling.java
+++ 01-java-fundamentals/06-puzzling/puzzling.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 public class puzzling {
 
+    // rolls
     public ArrayList<Integer>getRolls(){
         ArrayList<Integer> rolls = new ArrayList<Integer>(); 
         Random r = new Random();
@@ -14,17 +15,46 @@ public class puzzling {
         return rolls;
         }
     
-    // Alphabet
-    public ArrayList<Object> getAlphabet(){
-        ArrayList<Object> alphabet = new ArrayList<Object>();{
-            Random r = new Random();
-            int randomNum = r.nextInt(27);  
-            for (char ch = 'a'; ch <= 'z'; ++ch) {
-                alphabet.add(ch);
+        // Alphabet
+        public String RandomString () {
+            String characters = "abcdefghijklmnopqrstuvwxyz";
+            String randomString = " "; //Empty string, filled with characters
+            int length = 1; //Length of string, can be set to any number or random
+
+            Random rand = new Random ();
+            char[] text = new char[length]; 
+
+            for (int i = 0; i < length; i++) {
+                text[i] = characters.charAt(rand.nextInt(characters.length()));
+                randomString += text[i];
             }
-            return alphabet;
+            return randomString;
         }
-    }
+
+        // Generate password
+        public String randomPassword () {
+            String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            String randomPass = " "; //Empty string, filled with characters
+            int length = 8; //Length of string, can be set to any number or random
+
+            Random rand = new Random (); 
+            char[] text = new char[length]; 
+
+            for (int i = 0; i < length; i++) {
+                text[i] = characters.charAt(rand.nextInt(characters.length()));
+                randomPass += text[i];
+            }
+            return randomPass;
+        }
+
+        // 8 random passwords
+        public ArrayList<String> multiplePasswords (int length) {
+            ArrayList<String> passwordChoices = new ArrayList<String> ();
+            for (int i = 0; i < length; i++) {
+                passwordChoices.add(randomPassword());
+            }
+            return passwordChoices;
+        }
 }
 
 
--- 01-java-fundamentals/06-puzzling/puzzlingTest.java
+++ 01-java-fundamentals/06-puzzling/puzzlingTest.java
@@ -3,16 +3,28 @@ import java.util.Random;
 import java.util.Collections;
 
 public class puzzlingTest {
-    public static void main(String[] args) {
+    private static char[] randomString;
+
+	public static void main(String[] args) {
         //Get Ten Rolls
         puzzling t = new puzzling();
         ArrayList<Integer> randRolls = t.getRolls();
         System.out.println(randRolls);
-        // Alphabet
-        // ArrayList<Object> getLetter = t.getAlphabet();
-        System.out.println(t.getAlphabet());
-        // random letter
 
+        // Alphabet / random letter
+        puzzling b = new puzzling();
+        String randNum = b.RandomString();
+        System.out.println(randNum);
+
+        //Generate Password
+        puzzling p = new puzzling();
+        String randPassword = p.randomPassword();
+        System.out.println(randPassword);
+
+        //Generate 8 Passwords
+        puzzling c = new puzzling();
+        ArrayList<String> randPasswords = c.multiplePasswords(8);
+        System.out.println(randPasswords);
     }
 }
 
